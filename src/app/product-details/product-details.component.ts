608575fa6fad04fb70c3240bf04b8e020af9eb0c
gheddie <stefan.schulz.1976@googlemail.com>
2020-05-14 16:18:15 +0200

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,51 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { products } from '../products';
-import { CartService } from '../cart.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
+import { CartService } from "../cart.service";
 
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-product;
+  info = "";
 
-info = "";
-
-constructor(private route: ActivatedRoute, private cartService: CartService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
-      this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")];
       this.draw();
     });
   }
 
   addToCart(product) {
     this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
+    window.alert("Your product has been added to the cart!");
   }
 
   /**
    * https://stackblitz.com/edit/angular-canvas-svg-gridfile=src%2Fapp%2Fapp.component.css
    */
 
-    textClicked() {
+  textClicked() {
     console.log("text clicked");
   }
 
   changeSize(size) {
     let val = size === -1 ? "" : `${size}px`;
-    d3.select(".mygrid")
-      .style("width", val);
+    d3.select(".mygrid").style("width", val);
   }
 
   getSize() {
-    
     let s1 = d3.select(".child1").style("width");
     let s2 = d3.select(".child-svg").style("width");
     let s3 = d3.select(".child-canvas").style("width");
@@ -57,8 +57,7 @@ constructor(private route: ActivatedRoute, private cartService: CartService) {}
   }
 
   draw() {
-
-    console.log('drawing...');
+    console.log("drawing...");
 
     let selcanvas = d3.select(".child-canvas");
     let canvas = <HTMLCanvasElement>selcanvas.node();
@@ -73,10 +72,9 @@ constructor(private route: ActivatedRoute, private cartService: CartService) {}
     let ctx = canvas.getContext("2d");
 
     // remove all the svg children, get dim to draw
-    let selsvg = d3.select(".child-svg").on('mouseover',
-      function(d){
-        // console.log('hello i am the canvas!!');
-      });
+    let selsvg = d3.select(".child-svg").on("mouseover", function(d) {
+      // console.log('hello i am the canvas!!');
+    });
 
     selsvg.selectAll("*").remove();
 
@@ -87,15 +85,24 @@ constructor(private route: ActivatedRoute, private cartService: CartService) {}
 
     for (let entry of this.product.cars) {
       console.log(entry);
-            selsvg.append("rect")
-      .attr("x", `${actualX}`)
-      .attr("y", 40)
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", "black").on('mouseover',
-      function(d){
-        console.log('hello i am the brand :' + entry.name +  ', value: ' + entry.serial);
-      });
+
+      selsvg
+        .append("rect")
+        .attr("x", `${actualX}`)
+        .attr("y", 40)
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "black")
+        .on("mouseover", function(d) {
+          console.log(
+            "hello i am the brand :" + entry.name + ", value: " + entry.serial
+          );
+        }).on("click", function(d) {
+          alert(
+            "clicked: " + entry.name + ", value: " + entry.serial
+          );
+        });
+
       actualX += 20;
     }
 
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,37 +1,33 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: "Phone XL",
     price: 799,
-    description: 'A large phone with one of the best screens',
-    'cars':[ 
-      {name :'Ford', serial: '123'},
-      {name :'BMW', serial: '456'},
-      {name :'Austin', serial: '789'}
-      ]
+    description: "A large phone with one of the best screens",
+    cars: [
+      { name: "Ford", serial: "123" },
+      { name: "BMW", serial: "456" },
+      { name: "Austin", serial: "789" }
+    ]
   },
   {
-    name: 'Phone Mini',
+    name: "Phone Mini",
     price: 699,
-    description: 'A great phone with one of the best cameras',
-    'cars':[ 
-      {name :'Audi', serial: '543'},
-      {name :'Peugeot', serial: '111'}
-      ]
+    description: "A great phone with one of the best cameras",
+    cars: [{ name: "Audi", serial: "543" }, { name: "Peugeot", serial: "111" }]
   },
   {
-    name: 'Phone Standard',
+    name: "Phone Standard",
     price: 299,
-    description: '',
-      'cars':[ 
-      {name :'Hummer', serial: '11-22-33'},
-      {name :'Nissan', serial: '6754'},
-      {name :'Borgward', serial: '66-921'},
-      {name :'Mercedes', serial: '88-777'},
-      {name :'VW', serial: '111-22'}
-      ]
+    description: "",
+    cars: [
+      { name: "Hummer", serial: "11-22-33" },
+      { name: "Nissan", serial: "6754" },
+      { name: "Borgward", serial: "66-921" },
+      { name: "Mercedes", serial: "88-777" },
+      { name: "VW", serial: "111-22" }
+    ]
   }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
